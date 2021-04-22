a9d712e597761afe2252f141252810fb620f5048
planeset94 <planeset94@gmail.com>
2021-04-22 17:53:24 +0200

    Adding style to html

--- asset/JS/password.js
+++ asset/JS/password.js
@@ -1,15 +1,17 @@
 // Chiedi il Nome
 var nombre = prompt('Come ti chiami?');
+document.getElementById('nome_utente').innerHTML='Il tuo nome è: '+nombre;
 
 // chiedi il Cognome
 var surName = prompt("Qual'è il tuo cognome?");
+document.getElementById('cognome_utente').innerHTML='Il tuo cognome è: '+surName;
 // chiedi il colore preferito
 var color = prompt("Qual'è il tuo colore preferito?");
-
+document.getElementById('colore_utente').innerHTML='Il tuo colore preferito è: '+color;
 // Stampa sulla pagina il risultato
 
 
-var total= document.getElementById('pass').innerHTML='La tua nuova password è ' +nombre+surName+color+21;
+var total= document.getElementById('pass').innerHTML='La tua nuova password è: ' +nombre+surName+color+21;
 
 // Altro Modo 
 // var total= nombre+surName+color+21;

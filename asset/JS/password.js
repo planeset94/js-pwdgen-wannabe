6d61367b571d84d7bbb18573dcb1b731851a714f
planeset94 <planeset94@gmail.com>
2021-04-23 09:51:33 +0200

    ADD toLowercase

--- asset/JS/password.js
+++ asset/JS/password.js
@@ -1,13 +1,13 @@
 // Chiedi il Nome
 var nombre = prompt('Come ti chiami?');
-document.getElementById('nome_utente').innerHTML='Il tuo nome è: '+nombre;
+document.getElementById('nome_utente').innerHTML='Il tuo nome è: '+nombre.toUpperCase();
 
 // chiedi il Cognome
 var surName = prompt("Qual'è il tuo cognome?");
-document.getElementById('cognome_utente').innerHTML='Il tuo cognome è: '+surName;
+document.getElementById('cognome_utente').innerHTML='Il tuo cognome è: '+surName.toUpperCase();
 // chiedi il colore preferito
 var color = prompt("Qual'è il tuo colore preferito?");
-document.getElementById('colore_utente').innerHTML='Il tuo colore preferito è: '+color;
+document.getElementById('colore_utente').innerHTML='Il tuo colore preferito è: '+color.toUpperCase();
 // Stampa sulla pagina il risultato
 
 
@@ -19,4 +19,4 @@ document.getElementById('colore_utente').innerHTML='Il tuo colore preferito è: 
 
 // Altro Modo 
 var total= nombre+surName+color+21;
-document.getElementById('pass').innerHTML=total.toUpperCase();
+document.getElementById('pass').innerHTML=total.toLowerCase();
